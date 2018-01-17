0631b74b98140bf732a8b1ae042850d584aa81dc
Remi Liu <ljqx@live.cn>
2018-01-17 21:24:45 +0800

    add slider

--- src/components/Charts/TrendChart/index.js
+++ src/components/Charts/TrendChart/index.js
@@ -6,37 +6,91 @@ import autoHeight from '../autoHeight';
 
 @autoHeight()
 export default class TrendChart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange({ startValue, endValue }) {
+    this.ds.setState('start', new Date(startValue).getTime());
+    this.ds.setState('end',  new Date(endValue).getTime());
+  }
+
   render() {
     const {
       data = [],
     } = this.props;
 
-    const ds = new DataSet();
-    const dv = ds.createView().source(data);
-    dv.transform({
-      type: 'fold',
-      fields: ['Revenue', 'UserCount'],
-      key: 'key',
-      value: 'value',
+    if (data.length === 0) return null;
+
+    const ds = this.ds = new DataSet({
+      state: {
+        start: new Date(data[0].Date).getTime(),
+        end: new Date(data[data.length - 1].Date).getTime()
+      }
     });
+    const originDv = ds.createView('origin');
+    originDv.source(data)
+      .transform({
+        type: 'fold',
+        fields: [ 'Revenue', 'UserCount' ],
+        key: 'type',
+        value: 'value',
+        retains: [ 'Revenue', 'UserCount', 'Weather-PM25', 'Date' ]
+      });
+
+    const chartDv = ds.createView();
+    chartDv.source(data)
+      .transform({
+        type: 'fold',
+        fields: [ 'Revenue', 'UserCount' ],
+        key: 'type',
+        value: 'value',
+        retains: [ 'Revenue', 'UserCount', 'Weather-PM25', 'Date' ]
+      })
+      .transform({
+        type: 'filter',
+        callback(obj) {
+          const time = new Date(obj.Date).getTime(); // !注意：时间格式，建议转换为时间戳进行比较
+          return time >= ds.state.start && time <= ds.state.end;
+        }
+      });
 
     // https://alibaba.github.io/BizCharts/demo-detail.html?code=demo/other/rain-and-flow
     // comparing with Wheather? PM25 & Temprature
     return (
-      <Chart
-        height={400}
-        data={data}
-        scale={{ Date: { type: 'time' } }}
-        padding={[60, 40, 40, 60]}
-        forceFit>
-        <Axis name="Date" />
-        <Axis name="Revenue" />
-        <Axis name="UserCount" />
-        <Tooltip />
-        <Legend name="key" position="top" />
-        <Geom type="line" position="Date*Revenue" size={2} color="lightblue" />
-        <Geom type="line" position="Date*UserCount" size={2} color="lightgreen" />
-      </Chart>
+      <div>
+        <Chart
+          height={400}
+          data={chartDv}
+          scale={{ Date: { type: 'time' } }}
+          padding={[60, 80, 40, 60]}
+          forceFit>
+          <Axis name="Date" />
+          <Axis name="Revenue" title="Revenue" />
+          <Axis name="UserCount" title="User Count" />
+          <Tooltip />
+          <Legend />
+          <Geom type="line" position="Date*Revenue" size={2} color="lightblue" />
+          <Geom type="line" position="Date*UserCount" size={2} color="lightgreen" />
+        </Chart>
+        <div>
+          <Slider
+            width='auto'
+            height={26}
+            start={ds.state.start}
+            end={ds.state.end}
+            xAxis="Date"
+            yAxis="value"
+            scales={{Date:{type: 'time',tickCount: 10, mask:'M/DD' }}}
+            data={originDv}
+            backgroundChart={{type: 'line'}}
+            onChange={this.onChange}
+            padding={[60, 80, 40, 60]}
+          />
+        </div>
+      </div>
     );
   }
 }
