dda763cbb54242c62c73a3c8d7981c83eee442da
Yucong Jin <yucongj@microsoft.com>
2018-01-16 19:19:35 -0800

    rename title

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -94,9 +94,9 @@ class BasicLayout extends React.PureComponent {
   getPageTitle() {
     const { routerData, location } = this.props;
     const { pathname } = location;
-    let title = 'Ant Design Pro';
+    let title = 'Sedexo dashboard';
     if (routerData[pathname] && routerData[pathname].name) {
-      title = `${routerData[pathname].name} - Ant Design Pro`;
+      title = `${routerData[pathname].name} - Sedexo dashboard`;
     }
     return title;
   }
