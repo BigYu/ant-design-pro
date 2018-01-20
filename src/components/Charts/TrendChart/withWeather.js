6818b95fc85a9a5fc9a3904f57463e3a0f718949
Remi Liu <ljqx@live.cn>
2018-01-21 01:55:56 +0800

    thend card

---
+++ src/components/Charts/TrendChart/withWeather.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import { Chart, Tooltip, Geom, Legend, Axis, View, Facet } from 'bizcharts';
+import DataSet from '@antv/data-set';
+import Slider from 'bizcharts-plugin-slider';
+import { Checkbox, Row, Col, Switch } from 'antd';
+import autoHeight from '../autoHeight';
+import { isWorkday } from '../../../utils/utils';
+import WorkdaySwitch from '../WorkdaySwitch';
+
+@autoHeight()
+export default class TrendChartByWeather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ignoreWeekend: true,
+    };
+    this.onChangeIgnoreWeekend = this.onChangeIgnoreWeekend.bind(this);
+  }
+
+  onChangeIgnoreWeekend(ignoreWeekend) {
+    this.setState({
+      ignoreWeekend,
+    });
+  }
+
+  render() {
+    const {
+      data = [],
+      events,
+    } = this.props;
+
+    if (data.length === 0) return null;
+
+    const endDate = new Date(data[data.length - 1].Date);
+    const defaultStartDate = new Date(endDate.getFullYear(), endDate.getMonth(), 1);
+    const ds = this.ds = new DataSet({
+      state: {
+        start: defaultStartDate.getTime(),
+        end: endDate.getTime()
+      }
+    });
+
+    const chartDv = ds.createView();
+    chartDv.source(data)
+      .transform({
+        type: 'map',
+        callback(obj) {
+          obj.Revenue = Number(obj.Revenue);
+          obj.UserCount = Number(obj.UserCount);
+          obj.WeatherPM25 = Number(obj.WeatherPM25);
+          obj.WeatherAvgTemperature = Number(obj.WeatherAvgTemperature);
+          obj.Event = ((new Date(obj.Date) - 1) % 23 === 3) ? 1 : 0;
+          return obj;
+        }
+      })
+      // .transform({
+      //   type: 'fold',
+      //   fields: [ 'Revenue', 'UserCount', 'WeatherPM25', 'WeatherAvgTemperature' ],
+      //   key: 'type',
+      //   value: 'value',
+      //   retains: [ 'Revenue', 'UserCount', 'WeatherPM25', 'WeatherAvgTemperature', 'Date' ]
+      // })
+      .transform({
+        type: 'fold',
+        fields: [ 'Revenue', 'WeatherPM25' ],
+        key: 'type',
+        value: 'value',
+        retains: [ 'Revenue', 'UserCount', 'WeatherPM25', 'WeatherAvgTemperature', 'Event', 'Date' ]
+      });
+
+    if (this.state.ignoreWeekend) {
+      chartDv.transform({
+        type: 'filter',
+        callback: (item) => {
+          const date = new Date(item.Date);
+          return isWorkday(date);
+        }
+      })
+    }
+    const cols = {
+      Date: {
+        type: 'time',
+        tickCount: 10,
+        mask: 'M/DD H:mm'
+      }
+    }
+    // https://alibaba.github.io/BizCharts/demo-detail.html?code=demo/other/rain-and-flow
+    // comparing with Wheather? PM25 & Temprature
+    return (
+      <div>
+        <Row type='flex' justify='space-between' style={{ margin: '10px 60px' }}>
+          <WorkdaySwitch
+            checked={this.state.ignoreWeekend}
+            onChange={this.onChangeIgnoreWeekend}
+          />
+        </Row>
+        <Chart height={400} data={chartDv} scale={{
+            Date: { type: 'time' },
+            Revenue: { type: 'linear'},
+            WeatherPM25: { type: 'linear', alias: 'PM2.5' },
+          }} forceFit padding={[ 20, 80 ]}>
+          <Facet type="mirror" fields={[ 'type' ]} showTitle={false} padding={[ 0, 0, 40, 0]} eachView={(view, facet) => {
+            const { colValue, data } = facet;
+
+            if (colValue === 'Revenue') {
+              view.source(data, {
+                Revenue: {
+                  alias: '营业额'
+                },
+                UserCount: {
+                  alias: '客户数量'
+                }
+              });
+              view.axis('Revenue', {
+                title: {
+                  autoRotate: false,
+                  offset: -10,
+                  position: 'end',
+                  textStyle: {
+                    textAlign: 'start'
+                  }
+                }
+              });
+              view.axis('Event', false);
+              view.tooltip({
+                crosshairs: {
+                  type: 'line'
+                }
+              });
+              view.line().position('Date*Revenue').color('#4472c4');
+              view.line().position('Date*UserCount').color('#ed7d31');
+              view.interval().position('Date*Event').color('#255e91');
+            } else if (colValue === 'WeatherPM25') {
+              view.source(data, {
+                WeatherPM25: {
+                  alias: 'PM2.5'
+                },
+                WeatherAvgTemperature: {
+                  alias: '平均温度'
+                }
+              });
+              view.axis('WeatherPM25', {
+                title: {
+                  autoRotate: false,
+                  offset: -10,
+                  position: 'end',
+                  textStyle: {
+                    textAlign: 'start'
+                  }
+                }
+              });
+              view.area().position('Date*WeatherPM25').color('#a5a5a5');
+              view.line().position('Date*WeatherAvgTemperature').color('#ffc000');
+            }
+          }}/>
+        </Chart>
+      </div>
+    );
+  }
+}
